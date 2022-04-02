12f6b32c64ca89e1b3d096bc0a5302a19c3175e7
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-04-02 10:50:54 +0530

    Update button.js

--- modules/button.js
+++ modules/button.js
@@ -1,1 +1,12 @@
 
+const axios = require('axios');
+client.on('message', async msg => {
+    console.log('MESSAGE RECEIVED', msg);
+  if (msg.body === '!buttons') {
+        let button = new Buttons('Button body',[{body:'bt1'},{body:'bt2'},{body:'bt3'}],'title','footer');
+        client.sendMessage(msg.from, button);
+    } else if (msg.body === '!list') {
+        let sections = [{title:'sectionTitle',rows:[{title:'ListItem1', description: 'desc'},{title:'ListItem2'}]}];
+        let list = new List('List body','btnText',sections,'Title','footer');
+        client.sendMessage(msg.from, list);
+    }});
